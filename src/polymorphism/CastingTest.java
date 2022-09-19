7b0f05ef8abec6d931b18a51e5a46103dc07dab5
kiwon <kiwon.kim@seeroo.co.kr>
2022-09-19 17:01:05 +0900

    Polymorphism test

--- src/polymorphism/CastingTest.java
+++ src/polymorphism/CastingTest.java
@@ -29,6 +29,14 @@ public class CastingTest {
         st = (SmartTv)tv;
         st.channelUp();
 
+        /**
+         * FireEngine, Ambulance는 각각 Car Class를 상속 받았지만 서로는 아무런 관계가 아니다.
+         * Java에서 참조변수의 형변환은 상속관계에서만 가능하기 때문에 FireEngine, Ambulance는 형변환할 수 없다.
+         */
+        FireEngine f;
+        Ambulance a;
+//        a = (Ambulance)f; // 에러, 상속관계가 아닌 클래스간의 형변환 불가
+//        f = (FireEngine)a; // 에러 상속관계가 아닌 클래스간의 형변환 불가
 
 
 
--- src/polymorphism/CastingTest2.java
+++ src/polymorphism/CastingTest2.java
@@ -2,15 +2,17 @@ package polymorphism;
 
 public class CastingTest2 {
     public static void main(String[] args) {
+        Car car = new Car();
+        Car car2 = null;
+        FireEngine fe = null;
 
-        /**
-         * FireEngine, Ambulance는 각각 Car Class를 상속 받았지만 서로는 아무런 관계가 아니다.
-         * Java에서 참조변수의 형변환은 상속관계에서만 가능하기 때문에 FireEngine, Ambulance는 형변환할 수 없다.
-         */
-        FireEngine f;
-        Ambulance a;
-//        a = (Ambulance)f; // 에러, 상속관계가 아닌 클래스간의 형변환 불가
-//        f = (FireEngine)a; // 에러 상속관계가 아닌 클래스간의 형변환 불가
+        car.drive();
+        fe = (FireEngine)car; //실행시 에러 발생
+        fe.drive();
+        car2 = fe;
+        car2.drive();
+
+
 
     }
 }

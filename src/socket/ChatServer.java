7c0bb2045810d1ed9a00f97453d912fb571bdc7c
kiwon <kkw-11@naver.com>
2025-07-22 18:51:53 +0900

    refactor:Buffered 객체 변수 수정

--- src/socket/ChatServer.java
+++ src/socket/ChatServer.java
@@ -12,15 +12,15 @@ public class ChatServer {
         try (
                 ServerSocket serverSocket = new ServerSocket(PORT);
                 Socket clientSocket = serverSocket.accept();
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                BufferedReader bufferedIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter bufferedOut = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
                 Scanner scanner = new Scanner(System.in)
         ) {
             System.out.println("클라이언트1 연결됨: " + clientSocket.getInetAddress());
 
             while (true) {
                 // 1. 클라이언트 메시지 읽기
-                String clientMsg = in.readLine();
+                String clientMsg = bufferedIn.readLine();
                 if (clientMsg == null || clientMsg.equalsIgnoreCase("bye")) {
                     System.out.println("클라이언트 연결 종료");
                     break;
@@ -30,8 +30,8 @@ public class ChatServer {
                 // 2. 서버 메시지 입력 및 전송
                 System.out.print("[서버] 메시지 입력: ");
                 String serverMsg = scanner.nextLine();
-                out.write(serverMsg + "\n");
-                out.flush();
+                bufferedOut.write(serverMsg + "\n");
+                bufferedOut.flush();
                 System.out.println("[서버의 메시지 전송이 완료되었습니다.]\n");
 
                 if (serverMsg.equalsIgnoreCase("bye")) {

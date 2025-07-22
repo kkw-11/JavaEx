313c8455dda58c19eb6efb91bb07f6247f365bdb
kiwon <kkw-11@naver.com>
2025-07-22 19:17:48 +0900

    feat:키보드 입력 Buffered 방식으로 수정

--- src/socket/ChatClient.java
+++ src/socket/ChatClient.java
@@ -1,7 +1,10 @@
 package socket;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class ChatClient {
@@ -9,34 +12,24 @@ public class ChatClient {
         final String SERVER_IP = "127.0.0.1";
         final int SERVER_PORT = 12345;
 
-        try (
-                Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-                BufferedReader bufferedIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                BufferedWriter bufferedOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                Scanner scanner = new Scanner(System.in)
-        ) {
-            System.out.println("서버와 연결 : " + SERVER_IP);
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+
             while (true) {
-                // 1. 클라이언트 → 서버로 메시지 전송
-                System.out.print("[클라이언트] 메시지 입력: ");
-                String clientMsg = scanner.nextLine();
-                bufferedOut.write(clientMsg + "\n");
-                bufferedOut.flush();
-                System.out.println("[클라이언트 메시지 전송이 완료되었습니다.\n");
+                System.out.print("[클라이언트:] 서버로 보낼 메시지 입력: ");
+                String msg = in.readLine();
 
-                if (clientMsg.equalsIgnoreCase("bye")) {
-                    System.out.println("채팅 종료");
+
+                out.write(msg + "\n");
+                out.flush();
+                System.out.println("서버로 메시지를 전송했습니다. ");
+                if (msg == null || msg.equals("bye")) {
+                    System.out.println("채팅 프로그램을 종료 합니다. ");
                     break;
                 }
-
-                // 2. 서버 메시지 수신
-                String serverMsg = bufferedIn.readLine();
-                if (serverMsg == null || serverMsg.equalsIgnoreCase("bye")) {
-                    System.out.println("서버에서 채팅 종료");
-                    break;
-                }
-                System.out.println("[서버] " + serverMsg);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }

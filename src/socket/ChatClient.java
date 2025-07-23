cdfbcffaed85d7eb9cf6380d6afd43c9c770bf27
kiwon <kkw-11@naver.com>
2025-07-23 16:24:41 +0900

    feat:ChatClient UI/UX 개선, 닉네임 입력, 입장 알림 메세지 출력

--- src/socket/ChatClient.java
+++ src/socket/ChatClient.java
@@ -1,35 +1,36 @@
 package socket;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatClient {
     public static void main(String[] args) {
         final String SERVER_IP = "127.0.0.1";
         final int SERVER_PORT = 12345;
+        Scanner scanner = new Scanner(System.in);
 
-        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-             BufferedWriter networkOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-             BufferedReader keyboardInput = new BufferedReader(new InputStreamReader(System.in))) {
-
+        try (
+                Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
+        ) {
+            System.out.println("**채팅방에 입장했습니다.**\n**닉네임을 입력하세요**");
+            System.out.print("[닉네임 입력] : ");
+            String nickName = scanner.nextLine();
+            out.write(nickName + "\n");
+            out.flush();
             while (true) {
-                System.out.print("[클라이언트:] 서버로 보낼 메시지 입력: ");
-                String msg = keyboardInput.readLine();
-
-                networkOut.write(msg + "\n");
-                networkOut.flush();
-                System.out.println("서버로 메시지를 전송했습니다. ");
-                if (msg == null || msg.equals("bye")) {
-                    System.out.println("채팅 프로그램을 종료 합니다. ");
+                System.out.print("[" + nickName + "]" + "메시지 입력(bye 입력 시 종료):");
+                String msg = scanner.nextLine();
+                out.write(msg + "\n");
+                out.flush();
+                if ("bye".equalsIgnoreCase(msg.trim())) {
                     break;
                 }
             }
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
